f2de071a65ae06b01400aff780c10099ad31b9ee
sieunhanbom04 <31831447+sieunhanbom04@users.noreply.github.com>
2018-04-13 09:31:50 +0200

    Add files via upload

--- kahn.ml
+++ kahn.ml
@@ -190,9 +190,107 @@ module Pr: S = struct
     e' v ()
 
 end
+(*
+module Pr: S = struct
+  type 'a process = (unit -> 'a)
 
+  type 'a in_port = in_channel
+  type 'a out_port = out_channel
+
+  let new_channel () = let (fd_in, fd_out) = Unix.pipe () in
+    let in_channel = Unix.in_channel_of_descr fd_in in
+    let out_channel = Unix.out_channel_of_descr fd_out in
+    in_channel, out_channel
+
+  let put value out_channel () =
+    Marshal.to_channel out_channel value [Marshal.Compat_32];
+    flush out_channel
+
+  let rec get in_channel () =
+    try
+      let value = Marshal.from_channel in_channel
+      in
+      value
+    with End_of_file -> ();
+    get in_channel ()
+
+  let run e = e ()
+
+  let doco process_list () =
+    let rec process_handle l =
+      match l with
+      | [] -> ()
+      | processA :: [] -> run processA
+      | processA :: rest -> ( match fork () with
+                              | 0 -> run processA
+                              | _ -> process_handle rest
+                            )
+    in process_handle process_list
+
+  let return v = (fun () -> v)
+
+  let bind e e' () =
+    let v = e () in
+    e' v ()
+
+end
+*)
 (*Implementation with sequential simulation*)
 
+module Seq: S = struct
+  type 'a process = ('a -> unit) -> unit
+
+  (*type 'a channel = { q : 'a Queue.t; mutable ('a -> unit) option }*)
+  type 'a channel = { q : 'a Queue.t; w: ('a -> unit) Queue.t }
+  type 'a in_port = 'a channel
+  type 'a out_port = 'a channel
+
+  let queue_Process = Queue.create ()
+
+  let enqueue p = Queue.push p queue_Process
+
+  let dequeue () = Queue.pop queue_Process
+
+  exception Stop
+
+  let run (f : 'a process) : 'a  =
+    print_endline "start";
+    enqueue (fun () -> f (fun x -> ()));
+    (*try*)
+    while true do
+      (*print_endline "shit";*)
+      dequeue () ()
+    done;
+    raise Stop
+
+
+  let doco (l : (unit process) list) : (unit process) =
+    (fun x -> print_endline "LOL"; List.iter (fun f -> enqueue (fun () -> (f (fun () -> ())))) l)
+
+  let new_channel () = let c = { q=Queue.create (); w= Queue.create () } in c, c
+
+  let rec get f continuation =
+    match (Queue.is_empty f.q), (Queue.is_empty f.w) with
+
+    (*The value queue is empty, keep the continuation for later*)
+    | true, _ -> Queue.add continuation f.w
+
+    (*The value queue is not empty, the funcion queue is empty*)
+    | false, true -> continuation (Queue.take f.q)
+
+  let put v f continuation =
+    Queue.add v f.q;
+    match (Queue.is_empty f.w) with
+    | false -> enqueue (fun () -> (Queue.take f.w) (Queue.take f.q)); enqueue continuation
+    | true -> enqueue continuation
+
+  let return v = fun k -> k v
+
+  let bind e e' = fun k -> e (fun v -> e' v k)
+end
+
+(*Implementation with sequential simulation*)
+(*
 module Seq: S = struct
   type 'a process = ('a -> unit) -> unit
 
@@ -256,7 +354,7 @@ module Seq: S = struct
 
   let bind e e' = fun k -> e (fun v -> e' v k)
 end
-
+*)
 (*Original implementation by Thread*)
 
 module Th: S = struct
@@ -294,7 +392,7 @@ module Th: S = struct
   let return v = (fun () -> v)
 
   let bind e e' () =
-    let v = e ()
+    let v = e () in
     Thread.yield ();
     e' v ()
 
